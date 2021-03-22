5410e63c4e4d894bcf7b2d45564d65ba373f2ce6
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-22 07:09:17 +0300

    additional keys for return object in getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,19 @@ export async function getStaticProps() {
     const filePath = path.join(process.cwd(),'data','dummy-data.json');
     const stringifiedData = await fs.readFile(filePath);
     const data = JSON.parse(stringifiedData);
+
+    //returns a 404 page
+    if(!data) {
+        return { notFound: true }
+    }
+
+    //redirect to a certain page
+    if(data.length===0) {
+        return { redirect: {
+            destination: '/'
+        } }
+    }
+
     return {props: {
         items: data.items
     },
