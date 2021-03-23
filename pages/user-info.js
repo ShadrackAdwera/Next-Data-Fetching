ddab50a596d905908319aa16f36c77c408d8c129
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-23 06:18:41 +0300

    use getServerSideProps for data fetching

--- pages/user-info.js
+++ pages/user-info.js
@@ -8,7 +8,11 @@ const UserInfo = props => {
 }
 
 //similar to getStaticProps but revalidate key cannot be set here
+//Other differences include the kind of data you have access to in the context
 export async function getServerSideProps(context) {
+
+    const { params, req, res } = context;
+
     const userDetails = {
         userName: 'Deez Nuts',
         department: 'ICT Department',
